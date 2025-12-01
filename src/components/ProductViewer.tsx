19623ca941244c310bb42553377178e748656dfa
durraniarts <abdurrehmandurrani1215@gmail.com>
2025-12-01 17:19:36 +0500

    Lights

--- src/components/ProductViewer.tsx
+++ src/components/ProductViewer.tsx
@@ -3,6 +3,7 @@ import useMacbookStore from "../store";
 import { Canvas } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
 import MacbookModel14 from "./models/Macbook-14";
+import StudioLights from "./StudioLights";
 
 const ProductViewer = () => {
   const { color, scale, setColor, setScale } = useMacbookStore();
@@ -62,11 +63,12 @@ const ProductViewer = () => {
         id="canvas"
         camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
       >
+        <StudioLights />
         <ambientLight intensity={1} />
         {/* <Box position={[0, 0, 0]} scale={10 * scale} material-color={color}>
         </Box> */}
         <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
-        <OrbitControls enableZoom={true} />
+        <OrbitControls enableZoom={false} />
       </Canvas>
     </section>
   );
